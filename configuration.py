975bf4c84ae82355cd37a5f3693a6d4915e6cc70
Arkadiy <arkaal@yandex.ru>
2023-12-01 13:46:59 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://a462e63c-b4d7-4e9a-a86f-c919828669a5.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
